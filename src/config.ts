41c13460d1c050de4e65a96e1c13c149f54ec395
minaiice <1403486653@qq.com>
2026-01-18 00:53:30 +0800

    update blog

--- src/config.ts
+++ src/config.ts
@@ -100,9 +100,9 @@ export const siteConfig: SiteConfig = {
 		// 支持单张图片或图片数组，当数组长度 > 1 时自动启用轮播
 		src: {
 			desktop: [
-				"/assets/desktop-banner/66ccff.png",
-				"/assets/desktop-banner/66ccff0.png",
-				"/assets/desktop-banner/66ccff1.png",
+				"/assets/desktop-banner/66ccff.webp",
+				"/assets/desktop-banner/66ccff0.webp",
+				"/assets/desktop-banner/66ccff1.webp",
 			], // 桌面横幅图片
 			mobile: [
 				"/assets/mobile-banner/1.png",
@@ -207,9 +207,9 @@ export const siteConfig: SiteConfig = {
 export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 	src: {
 		desktop: [
-			"/assets/desktop-banner/66ccff.png",
-			"/assets/desktop-banner/66ccff0.png",
-			"/assets/desktop-banner/66ccff1.png",
+			"/assets/desktop-banner/66ccff.webp",
+			"/assets/desktop-banner/66ccff0.webp",
+			"/assets/desktop-banner/66ccff1.webp",
 		], // 桌面横幅图片
 		mobile: [
 			"/assets/mobile-banner/1.png",
@@ -432,8 +432,8 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 };
 
 export const footerConfig: FooterConfig = {
-	enable: false, // 是否启用Footer HTML注入功能
-	customHtml: "", // HTML格式的自定义页脚信息，例如备案号等，默认留空
+	enable: true, // 是否启用Footer HTML注入功能
+	customHtml: "<a href=\"https://icp.gov.moe/?keyword=20252047\" target=\"_blank\">萌ICP备20252047号</a>", // HTML格式的自定义页脚信息，例如备案号等，默认留空
 	// 也可以直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
 	// 注意：若 customHtml 不为空，则使用 customHtml 中的内容；若 customHtml 留空，则使用 FooterConfig.html 文件中的内容
 	// FooterConfig.html 可能会在未来的某个版本弃用
